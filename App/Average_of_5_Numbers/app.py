4626e9762c6b03409673e5f4e8c779eb83b4a907
naomietb <naomietb@gmail.com>
2023-05-19 20:37:05 -0500

    End of Lesson 2

---
+++ App/Average_of_5_Numbers/app.py
@@ -0,0 +1,8 @@
+firstnumber = 1
+secondnumber = 2
+thirdnumber = 3
+fourthnumber = 4
+fifthnumber = 5
+total = firstnumber + secondnumber + thirdnumber + fourthnumber + fifthnumber
+average = total / 5
+print(average)
